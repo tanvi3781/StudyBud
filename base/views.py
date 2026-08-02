e863ae76ad34beae7dcdcdb30d57977d088a5e1a
Tanvi Pattewar <tanvipattewar@Tanvis-MacBook-Pro.local>
2026-08-02 15:04:13 +0530

    All the functionalities of the project along with Responsive and Mobile Support UI is done

--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room , Topic , Message
-from .forms import RoomForm
+from .forms import RoomForm,UserForm
 # Create your views here.
 # rooms=[
 #     {'id':1,'name':'Lets learn python!'},
@@ -72,7 +72,7 @@ def home(request):
         Q(name__icontains=q) |
         Q(description__icontains=q)
     )
-    topics=Topic.objects.all()
+    topics=Topic.objects.all()[0:5]
     room_count=rooms.count()
     room_messages=Message.objects.filter(Q(room__topic__name__icontains=q))
 
@@ -102,32 +102,59 @@ def room(request,pk):
                'participants': participants}
     return render(request, 'base/room.html',context)
 
+
+def userProfile(request,pk):
+    user=User.objects.get(id=pk)
+    rooms=user.room_set.all()
+    room_messages=user.message_set.all()
+    topics=Topic.objects.all()
+    context={'user': user,'rooms':rooms,'room_messages':room_messages,'topics':topics}
+    return render(request, 'base/profile.html',context)
+
+
 @login_required(login_url='login')
 def createRoom(request):
     form=RoomForm()
+    topics=Topic.objects.all()
     if request.method=='POST':
-        form=RoomForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    context={'form': form}
+        topic_name=request.POST.get('topic')
+        topic, created=Topic.objects.get_or_create(name=topic_name)
+
+        Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get('name'),
+            description=request.POST.get('description'),
+        )
+        # form=RoomForm(request.POST)
+        # if form.is_valid():
+        #     room=form.save(commit=False)
+        #     room.host=request.user
+        #     room.save()
+        return redirect('home')
+
+    context={'form': form, 'topics': topics}
     return render(request, 'base/room_form.html',context)
     
 @login_required(login_url='login')
 def updateRoom(request,pk):
     room=Room.objects.get(id=pk)
     form=RoomForm(instance=room)
+    topics=Topic.objects.all()
 
     if request.user != room.host:
         return HttpResponse('You are not allowed here!!')
 
     if request.method=='POST':
-        form=RoomForm(request.POST,instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+        topic_name=request.POST.get('topic')
+        topic, created=Topic.objects.get_or_create(name=topic_name)
+        room.name=request.POST.get('name')
+        room.topic=topic
+        room.description=request.POST.get('description')
+        room.save()
+        return redirect('home')
 
-    context={'form': form}
+    context={'form': form, 'topics': topics, 'room': room}
     return render(request, 'base/room_form.html',context)
 
 @login_required(login_url='login')
@@ -153,3 +180,26 @@ def deleteMessage(request,pk):
         message.delete()
         return redirect('home')
     return render(request, 'base/delete.html',{'obj': message})
+
+@login_required(login_url='login')
+def updateUser(request):
+    user=request.user
+    form=UserForm(instance=user)
+
+    if request.method=='POST':
+        form=UserForm(request.POST,instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile',pk=user.id)
+            
+    return render(request, 'base/update-user.html',{'form': form})
+
+def topicsPage(request):
+    q=request.GET.get('q') if request.GET.get('q')!=None else ''
+    topics=Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html',{'topics': topics})    
+
+
+def activityPage(request):
+    room_messages=Message.objects.all()
+    return render(request, 'base/activity.html',{'room_messages': room_messages})   
--- studybud/settings.py
+++ studybud/settings.py
@@ -118,4 +118,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+
+
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+
+
+
