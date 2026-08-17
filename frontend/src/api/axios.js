55b95b77d181e6d4276bc8197f1108f56fc02a4a
Tanvi Pattewar <tanvipattewar@Tanvis-MacBook-Pro.local>
2026-08-17 18:46:39 +0530

    Api fixed

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://3.107.55.35:8000",
+  baseURL: import.meta.env.VITE_API_URL || "/api/",
 });
 
 api.interceptors.request.use(
